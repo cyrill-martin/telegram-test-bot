c6554f46dd969fd52d29550708e3585aeabbfcab
Cyrill Martin <cyrill.martin@hey.com>
2021-08-23 10:23:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 
 import os
 import logging
+import time
 from dotenv import load_dotenv
+from telegram import ChatAction
 from telegram.ext import Updater, CommandHandler
 
 # Load your environment variables from the .env file
@@ -12,31 +14,48 @@ bot_token = os.environ.get("BOT_TOKEN")
 bot_url = os.environ.get("BOT_URL")
 bot_mode = os.environ.get("BOT_MODE")
 
-# Create telegram bot handlers
+# Get chat-ID from update
+def get_chat_id(update):
+  return update.effective_chat.id
+
+# Mimic typing of the bot
+def show_typing(context, chat_id): 
+  context.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING, timeout=1)
+  time.sleep(1)
+
+# Send the actual reply
+def send_reply(context, chat_id, text):
+  show_typing(context, chat_id)
+  context.bot.send_message(chat_id=chat_id, text=text)
+
+################################
+# Create telegram bot handlers #
+################################
 def start(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, 
-                           text="Hello, I'm your telegram test bot.")
+  reply = "Hello, I'm your telegram test bot."
+  send_reply(context, get_chat_id(update), reply)
 
 def test(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id,
-                           text="Congrats, you tested the bot - again!")
+  reply = "Congrats, you tested the bot."
+  send_reply(context, get_chat_id(update), reply)
 
 def cool(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id,
-                           text="Yes, that's cool.")
+  reply = "Yes, that's cool."
+  send_reply(context, get_chat_id(update), reply)
 
 def sure(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id,
-                           text="I'm sure.")
+  reply = "I'm sure."
+  send_reply(context, get_chat_id(update), reply)
 
 def sk8(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id,
-                           text="Do a kickflip!")
+  reply = "Do a kickflip!"
+  send_reply(context, get_chat_id(update), reply)
 
 def main():
   # Create the telegram bot
   updater = Updater(token=bot_token, use_context=True)
   dispatcher = updater.dispatcher
+
   logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
   
   # Set the bot handlers
